09f5909f84fa711de74f6bc82fd7a047b1b1eaf5
Javid-C <javidia@code.edu.az>
2023-01-17 17:49:49 +0400

    JS intro

---
+++ 14. JS intro/assets/js/script.js
@@ -0,0 +1,113 @@
+// firstname = "Albert";
+
+// firstname = "Rashid";
+
+// console.log(fname);
+
+// //hoist
+// var fname = "Ulviyya";
+
+// var nullable = null;
+// var undefinedd = undefined;
+// var boolean = true;
+// var number = 5;
+// var bigint = 200n;
+// var string = "test";
+
+// console.log(typeof typeof nullable);
+// console.log(typeof typeof undefinedd);
+// console.log(typeof typeof boolean);
+// console.log(typeof typeof number);
+// console.log(typeof typeof bigint);
+// console.log(typeof typeof string);
+
+// let number = 10;
+// let number1 = 20;
+// console.log(number1);
+
+// console.log(number);
+// const number = 10;
+// number = 20;
+
+let arr = [
+  1,
+  2,
+  3,
+  4,
+  5,
+  "Albert",
+  true,
+  false,
+  null,
+  undefined,
+  { name: "Albert" },
+];
+
+// for (let i = arr.length; i >= 0; i--) {
+//   let element = arr[i];
+//   console.log(element);
+// }
+
+// for (let element in arr) {
+//   console.log(element);
+// }
+
+// let dayStr = prompt("Please enter day value");
+// let day = parseInt(dayStr, 2);
+// console.log(day);
+
+// switch (day) {
+//   case 1:
+//   case 2:
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thursday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("Please enter valid number");
+//     break;
+// }
+
+// let result = confirm("Are you sure about that");
+
+// if (result) {
+//   console.log("You entered yes");
+//   prompt("please enter valid number");
+// } else {
+//   console.log("You clicked no");
+// }
+
+// try {
+//   const number = 5;
+//   number = 10;
+// } catch (error) {
+//   throw new Error("This is new error message");
+// } finally {
+//   console.log("after catch");
+// }
+
+// var num1 = "test";
+
+// var num2 = 10;
+// let result = num2 / num1; //NaN
+
+// if (-Infinity) {
+//   console.log("if condition");
+// } else {
+//   console.log("else condition");
+// }
+
+let data = 5 && 1 && 1 && "";
+
+console.log(data);
