36b14d17f56355e9f665b439170f1f8e9721bd15
Javid-C <javidia@code.edu.az>
2023-02-06 17:50:18 +0400

    Basket practice

--- 23. FileReader, Storage/assets/js/script.js
+++ 23. FileReader, Storage/assets/js/script.js
@@ -1,5 +1,8 @@
 let fileInput = document.getElementById("file");
 let images = document.querySelector(".images");
+let undo = document.querySelector(".undo");
+
+let lastImages = [];
 
 fileInput.addEventListener("change", function (e) {
   let files = Array.from(e.target.files);
@@ -29,12 +32,6 @@ function ShowImage(file) {
     closeButton.innerText = "X";
     closeButton.style.position = "absolute";
     closeButton.style.right = "0px";
-    closeButton.addEventListener("click", function () {
-      let result = confirm("Are you sure to delete this photo?");
-      if (result) {
-        image.remove();
-      }
-    });
     image.style.position = "relative";
     image.style.width = "150px";
     image.style.height = "150px";
@@ -46,5 +43,22 @@ function ShowImage(file) {
     image.append(img);
     image.append(closeButton);
     images.appendChild(image);
+    closeButton.addEventListener("click", function () {
+      let result = confirm("Are you sure to delete this photo?");
+      if (result) {
+        lastImages.push(image);
+        image.remove();
+        undo.style.display = "inline-block";
+      }
+    });
   });
 }
+
+undo.addEventListener("click", function () {
+  if (lastImages.length) {
+    images.append(lastImages.pop());
+    if (lastImages.length == 0) {
+      undo.style.display = "none";
+    }
+  }
+});
--- 24. Basket/assets/js/script.js
+++ 24. Basket/assets/js/script.js
@@ -1,4 +1,5 @@
 let titles = document.querySelectorAll(".title");
+let basketHTML = document.querySelector("#Basket .basket");
 document.addEventListener("DOMContentLoaded", function () {
   let basketStr = localStorage.getItem("basket"); //"[]"
   let basket = JSON.parse(basketStr); //[]
@@ -38,6 +39,9 @@ buttons.forEach((button) => {
     }
     ShowProductCount(basket);
     ShowTotalPrice(basket);
+    AddToBasket(product);
+
+    // console.log(basket);
     let basketStr = JSON.stringify(basket);
     localStorage.setItem("basket", basketStr);
   });
@@ -67,3 +71,64 @@ function ShowTotalPrice(basket) {
     return (total += parseInt(product.price * product.count));
   }, 0);
 }
+
+function AddToBasket(product) {
+  let baskets = document.querySelector(".basket");
+  let div = document.createElement("div");
+  let image = document.createElement("img");
+  let price = document.createElement("span");
+  let closeBtn = document.createElement("button");
+
+  console.log(baskets);
+  closeBtn.className = "btn btn-danger";
+  closeBtn.innerText = "X";
+  closeBtn.style.position = "absolute";
+  closeBtn.style.right = "10px";
+  div.className = "basket-item col-lg-2";
+  div.style.position = "relative";
+  div.setAttribute("data-id", product.id);
+  image.src = product.src;
+  image.style.width = "100px";
+  price.innerText = product.price;
+  div.append(image, price, closeBtn);
+  basketHTML.append(div);
+  closeBtn.addEventListener("click", function () {
+    let basket = JSON.parse(localStorage.getItem("basket"));
+    if (!basket) {
+      localStorage.setItem("basket", JSON.stringify([]));
+      basket = JSON.parse(localStorage.getItem("basket"));
+    }
+    let id = this.parentElement.getAttribute("data-id");
+    let index = basket.findIndex((element) => {
+      return element.id == id;
+    });
+
+    delete basket[index];
+    basket = basket.filter(Object);
+    ShowProductCount(basket);
+    ShowTotalPrice(basket);
+    let basketStr = JSON.stringify(basket);
+    localStorage.setItem("basket", basketStr);
+    this.parentElement.remove();
+  });
+}
+
+// let deletes = document.querySelector(".delete");
+// let testBasket = [
+//   { id: 1, title: "test1" },
+//   { id: 2, title: "test2" },
+//   { id: 3, title: "test3" },
+//   { id: 4, title: "test4" },
+// ];
+
+// console.log(testBasket);
+// deletes.addEventListener("click", function () {
+//   let id = 3;
+
+//   let index = testBasket.findIndex((element) => {
+//     return element.id == id;
+//   });
+//   delete testBasket[index];
+//   testBasket = testBasket.filter(Object);
+//   console.log(testBasket);
+// });
